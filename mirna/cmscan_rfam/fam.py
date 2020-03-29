5782eead436feccbb40ae79d0f484f9fd20296a3
shyaman_madhawa <shayaman321@gmail.com>
2020-03-29 17:32:43 +0530

    new fam

--- mirna/cmscan_rfam/fam.py
+++ mirna/cmscan_rfam/fam.py
@@ -1,4 +1,5 @@
 import re
+import pandas as pd
 import numpy as np
 import sys, getopt
 
@@ -23,15 +24,18 @@ print ('Output file is :', outputfile)
 
 file1 = open(inputfile, 'r') 
 Lines = file1.read().splitlines() 
-header = re.split(r'\s+',Lines[0],18)
+header = ['target_name', 'accession', 'query_name', 'accession', 'mdl', 'mdl_from', 'mdl_to', 'seq_from', 'seq_to', 'strand', 'trunc', 'pass', 'gc', 'bias', 'score', 'E-value', 'inc', 'description_of_target']
 fam = []
 # Strips the newline character 
 for line in Lines[1:]: 
-    fam.append(re.split(r'\s+',line,17))
+    if(not line.startswith('#')):
+        fam.append(re.split(r'\s+',line,17))
 
 fam  = pd.DataFrame(fam,columns=header)
 fam['E-value'] = pd.to_numeric(fam['E-value'])
 idx = fam.groupby(['query_name'])['E-value'].transform(min) == fam['E-value']
 fam = fam[idx][['query_name','target_name']]
 fam = fam.rename(columns={'query_name': 'ID','target_name':'family'})
+fam['species'] = fam['ID'].str.split('-',n=1,expand=True)[0]
+
 fam.to_excel(outputfile,index=False)
