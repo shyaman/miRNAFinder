7b176308ad4056aab015f5b4f8b60fb30d4a8f0d
shyaman <shayaman321@gmail.com>
2020-05-22 14:10:11 +0530

    script to calculate feat at once

---
+++ mirna/calcfeat.sh
@@ -0,0 +1,4 @@
+(cd microPred && ./calculateHere.sh $1 $2_micropred)
+(cd motif && python3 motiffeat.py -p pos/meme.xml -n neg/meme.xml -i ../microPred/data/$1 -o $2_motif )
+(cd triplet && python3 triplet_elements.py -i ../microPred/data/$1 -o $2_triplet)
+python3 combine.py -m microPred/$2_micropred.xlsx -n motif/$2_motif.xlsx -t triplet/$2_triplet.xlsx -o $2_feat
